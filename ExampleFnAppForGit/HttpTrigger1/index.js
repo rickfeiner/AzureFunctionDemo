8e7f271ce0e3ba6eb7b9d7e5a2eb3413560bfc3c
Richard Feiner <richardfeiner@C02TW1KXHV2G.localdomain>
2019-03-25 10:30:34 -0500

    CI/CD Test

--- ExampleFnAppForGit/HttpTrigger1/index.js
+++ ExampleFnAppForGit/HttpTrigger1/index.js
@@ -4,6 +4,7 @@ module.exports = async function (context, req) {
     if (req.query.name || (req.body && req.body.name)) {
         context.res = {
             // status: 200, /* Defaults to 200 */
+            // Testing CI/CD
             body: "Hello " + (req.query.name || req.body.name)
         };
     }
